750f389d17e8c637e36579acb126b3d58b27b9ad
Rolf M <rolfie@outlook.dk>
2022-11-02 11:53:32 +0100

    Fetch API
    Henter Api

---
+++ assets/script/script.js
@@ -0,0 +1,12 @@
+const dataArr = [];
+
+fetch("https://api.tvmaze.com/shows")
+	.then((response) => response.json())
+
+	.then((data) => dataArr.push(...data))
+
+	.catch((error) => {
+		console.error(error);
+	});
+
+console.log(dataArr);
