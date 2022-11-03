41c912eeb3232bed4a91bf2fc886c57fb7bba55d
Rasmus Jensen <rasmusdj96@gmail.com>
2022-11-03 08:58:48 +0100

    Made the filmcontainers scrollable

--- assets/script/script.js
+++ assets/script/script.js
@@ -45,12 +45,46 @@ const generateGenres = () => {
 				container.innerHTML += `
                 <div class="card">
 					<div class = "cardContent">
-					<img src="${film.image.medium}">
+					<img src="${film.image.medium}" draggable="false">
 						${film.name}
 					</div>
 				</div>
                 `;
 			}
+		});
+	});
+	scrollableDiv();
+};
+
+//scrollable containers
+const scrollableDiv = () => {
+	const filmContainer = document.querySelectorAll(".filmContainer");
+	let isDown = false;
+	let startX;
+	let scrollLeft;
+
+	filmContainer.forEach((slider) => {
+		slider.addEventListener("mousedown", (e) => {
+			isDown = true;
+			slider.classList.add("active");
+			startX = e.pageX - slider.offsetLeft;
+			scrollLeft = slider.scrollLeft;
+		});
+		slider.addEventListener("mouseleave", () => {
+			isDown = false;
+			slider.classList.remove("active");
+		});
+		slider.addEventListener("mouseup", () => {
+			isDown = false;
+			slider.classList.remove("active");
+		});
+		slider.addEventListener("mousemove", (e) => {
+			if (!isDown) return;
+			e.preventDefault();
+			const x = e.pageX - slider.offsetLeft;
+			const walk = (x - startX) * 3; //scroll-fast
+			slider.scrollLeft = scrollLeft - walk;
+			// console.log(walk);
 		});
 	});
 };
